8c4c4ea8ab4933058c17290d954fc8e3e19d9404
deafnv <rokiniri@yahoo.in>
2022-12-26 01:37:31 +0800

    feat: reorder ptw proof of concept

--- pages/ptw.tsx
+++ pages/ptw.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import axios from 'axios'
 import { google } from 'googleapis'
-import { BaseSyntheticEvent, useEffect, useState } from 'react'
+import { BaseSyntheticEvent, useEffect, useRef, useState } from 'react'
 import { GetServerSidePropsContext } from 'next';
+import { distance, motion, useMotionValue } from 'framer-motion';
+import { clamp } from '@popmotion/popcorn';
+import { arrayMoveImmutable, arrayMoveMutable } from 'array-move'
 
 export const getStaticProps = async (context: GetServerSidePropsContext) => {
   const auth = await google.auth.getClient({ credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!), scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
@@ -46,6 +49,8 @@ export default function Home({ res }: {res: {id: number, title: string}[]}) {
   const [response, setResponse] = useState<{id: number, title: string}[]>(res);
   const [sortMethod, setSortMethod] = useState<string>('');
   const [isEdited, setIsEdited] = useState<any>(null);
+  const [isDragging, setDragging] = useState(false);
+  const [draggingIndex, setDraggingIndex] = useState<number>();
 
   useEffect(() => {
     document.addEventListener('click', (e: any) => {
@@ -53,14 +58,14 @@ export default function Home({ res }: {res: {id: number, title: string}[]}) {
       setIsEdited(null);
     })
 
-    const retrieveUpdates = setInterval(async () => {
+    /* const retrieveUpdates = setInterval(async () => {
       const updateResponse = await axios.get('/api/getupdates/ptw');
       setResponse(updateResponse.data);
     }, 3000);
     
     return () => {
       clearInterval(retrieveUpdates);
-    };
+    }; */
   },[])
 
   const sortListByName = (name: string) => {
@@ -116,6 +121,43 @@ export default function Home({ res }: {res: {id: number, title: string}[]}) {
     return <form onSubmit={handleSubmit}><input autoFocus type='text' defaultValue={ogvalue} className='input-text text-center w-4/5'></input></form>
   }
 
+  const buffer = 5;
+
+  const findIndex = (
+    i: number,
+    yOffset: number,
+    delta: number,
+    e: any
+  ) => {
+    let target = i;
+    const prevSibling = e.target.parentNode.previousSibling;
+    const nextSibling = e.target.parentNode.nextSibling;
+
+    // If moving down
+    if (delta > 0) {
+      if (!e.target.parentNode.nextSibling) return i;
+
+      const swapThreshold = nextSibling?.getBoundingClientRect().y + nextSibling?.offsetHeight / 2 + buffer;
+      if (yOffset > swapThreshold) target = i + 1;
+  
+      // If moving up
+    } else if (delta < 0) {
+      if (e.target.parentNode.previousSibling?.childNodes[0].tagName === 'TH') return i;
+
+      const swapThreshold = prevSibling?.getBoundingClientRect().y + prevSibling?.offsetHeight / 2 + buffer;
+      if (yOffset < swapThreshold) target = i - 1;
+    }
+  
+    return clamp(0, response.length, target);
+  };
+
+  const moveItem = (i: number, dragOffset: number, delta: number, e: MouseEvent | TouchEvent | PointerEvent) => {
+    const targetIndex = findIndex(i, dragOffset, delta, e);
+    if (targetIndex !== i) {
+      setResponse(arrayMoveImmutable(response, i, targetIndex));
+    }
+  };
+
   return (
     <>
       <Head>
@@ -132,13 +174,33 @@ export default function Home({ res }: {res: {id: number, title: string}[]}) {
             <tr>
               <th onClick={() => sortListByName('title')} className='w-[30rem] cursor-pointer'><span>Title</span><span className='absolute'>{sortSymbol('title')}</span></th>
             </tr> 
-            {response.map(item => {
-              return <tr key={item.id}>
-                <td onDoubleClick={() => {setIsEdited(`title${item.id}`)}}>{isEdited == `title${item.id}` ? editForm('title', item.id, item.title) : item.title}</td>
-              </tr>
+            {response.map((item, i) => {
+              return ( 
+                <motion.tr 
+                  key={item.id} 
+                  initial={false}
+                  style={sortMethod ? undefined : {cursor: 'move'}}
+                  animate={draggingIndex === i ? {zIndex: 100} : {zIndex: 0}}
+                  drag={sortMethod ? false : 'y'} 
+                  dragConstraints={{top: 0, bottom: 0}} 
+                  dragElastic={1} 
+                  onDragStart={() => {setDragging(true); setDraggingIndex(i)}}
+                  onDragEnd={() => {setDragging(false); setDraggingIndex(-1)}}
+                  onDrag={(e, { point, delta }) => moveItem(i, point.y, delta.y, e)}
+                  layout={true}
+                >
+                  <td onDoubleClick={() => {setIsEdited(`title${item.id}`)}}><span className='cursor-text'>{isEdited == `title${item.id}` ? editForm('title', item.id, item.title) : item.title}</span></td>
+                </motion.tr>
+              )
             })}
           </tbody>
         </table>
+        {response != res && !sortMethod ? 
+          <div className='flex gap-2 my-2'>
+            <button className='input-submit p-2 rounded-md' onClick={() => {}}>Save Changes</button>
+            <button className='input-submit p-2 rounded-md' onClick={() => {setResponse(res)}}>Cancel</button>
+          </div>
+         : null}
       </main>
     </>
   )
