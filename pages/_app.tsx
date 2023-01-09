6ed024d528b6cf92ca1ed6f0822eec3cf6def4d2
deafnv <rokiniri@yahoo.in>
2023-01-10 02:05:29 +0800

    feat: loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "../styles/nprogress.css";
+import NProgress from 'nprogress'
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  NProgress.configure({
+    showSpinner: false
+  })
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+ 
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteDone);
+    router.events.on("routeChangeError", handleRouteDone);
+ 
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteDone);
+      router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <Navbar>
       <Component {...pageProps} />
