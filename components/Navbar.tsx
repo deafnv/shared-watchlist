e049e0ce32a29e915e2bc6cb6dc6493838b35ec6
deafnv <rokiniri@yahoo.in>
2023-01-18 19:32:36 +0800

    feat: show function timer in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,9 @@
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Navbar({children}: React.PropsWithChildren) {
+  const [timer, setTimer] = useState<string>("1970-01-01T11:18:58.453Z") 
   const navLinks = [
     {
       name: 'Watched',
@@ -15,14 +18,22 @@ export default function Navbar({children}: React.PropsWithChildren) {
       route: '/seasonal'
     },
   ]
+
+  useEffect(() => {
+    async function getTimer() {
+      axios.get('http://update.ilovesabrina.org:3005/timer').then(response => setTimer(response.data))
+    }
+    getTimer();
+  },[])
   
-  return (
+  return (  
     <>
       <nav className="h-[60px] flex items-center justify-center gap-[20%] bg-black border-b-[1px]" style={{
         borderImage: 'linear-gradient(to right, rgb(218, 51, 190), rgb(191, 94, 255))',
         borderImageSlice: 1
       }}>
         <div className="flex items-center">
+          <span className='absolute left-8'>{new Intl.DateTimeFormat('en-GB', {dateStyle: 'medium', timeStyle: 'long'}).format(new Date(timer!))}</span>
           <ul>
             {navLinks.map((link, index) => {
               return (
