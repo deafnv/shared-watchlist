9944d6c56cc5a484d82be09f531636855716acc7
deafnv <rokiniri@yahoo.in>
2023-02-16 17:06:41 +0800

    feat: better socket test

--- pages/socket.tsx
+++ pages/socket.tsx
@@ -1,36 +1,35 @@
-import { useEffect, useState, BaseSyntheticEvent } from 'react'
+import { useEffect, BaseSyntheticEvent, useRef } from 'react'
 import { io, Socket } from 'socket.io-client'
 
 let socket: Socket
 
 export default function SocketTest() {
-  const [input, setInput] = useState('')
+  const input = useRef<HTMLInputElement>(null)
+  const setInput = (value: string) => input.current!.value = value
 
   useEffect(() => {
+    const socketInitializer = async () => {
+      socket = io(process.env.NEXT_PUBLIC_API_URL!)
+      socket.on('connect', () => {
+        console.log('connected')
+      })
+
+      socket.on('input-change', msg => {
+        setInput(msg)
+      })
+    }
     socketInitializer()
   }, [])
-  
-  const socketInitializer = async () => {
-    socket = io(process.env.NEXT_PUBLIC_API_URL!)
-    socket.on('connect', () => {
-      console.log('connected')
-    })
 
-    socket.on('update-input', msg => {
-      setInput(msg)
-    })
-  }
-
-  const onChangeHandler = (e: BaseSyntheticEvent) => {
-    setInput(e.target.value)
+  const handleChange = (e: BaseSyntheticEvent) => {
     socket.emit('input-change', e.target.value)
   }
 
   return (
     <input
+      ref={input}
       placeholder='Enter a message'
-      value={input}
-      onChange={onChangeHandler}
+      onChange={handleChange}
       className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-3 text-lg bg-black'
     />
   )
